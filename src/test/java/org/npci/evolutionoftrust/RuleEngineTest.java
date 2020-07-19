e566c7533d1f9f50a06d15885e8636fc161db7a2
Dhruv Kapur <dhruvkapur91@gmail.com>
2020-07-19 13:01:19 +0530

    Fix. Fix the given in the test!

--- src/test/java/org/npci/evolutionoftrust/RuleEngineTest.java
+++ src/test/java/org/npci/evolutionoftrust/RuleEngineTest.java
@@ -4,13 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.npci.evolutionoftrust.MoveType.CHEAT;
 import static org.npci.evolutionoftrust.MoveType.CORPORATE;
 
 public class RuleEngineTest {
     @Test
     void shouldHaveScoreOfDoubleTwoWhenBothPlayersCorporate() {
-        Score score = new RuleEngine(CORPORATE, CHEAT).score();
+        Score score = new RuleEngine(CORPORATE, CORPORATE).score();
         Score expectedScore = new Score(2, 2);
 
         assertThat(score, is(expectedScore));
